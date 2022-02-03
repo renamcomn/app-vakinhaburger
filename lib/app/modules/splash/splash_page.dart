018cdf763cc2e24303701a35c306108dd788e3bd
renamcomn <acidmud@gmail.com>
2022-02-03 15:28:10 -0300

    create widgets and splash page

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/utils.dart';
+import 'package:vakinha_burguer_mobile/app/core/ui/widgets/vakinha_button.dart';
 
 class SplashPage extends StatelessWidget {
 
@@ -7,8 +9,35 @@ class SplashPage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Container(),
+           body: Container(
+             color: Color(0xFF140E0E),
+             child: Stack(
+               children: [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    width: context.width,
+                    child: Image.asset('assets/images/lanche.png', fit: BoxFit.cover, )
+                  )
+                ),
+                Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.heightTransformer(reducedBy: 85),
+                  ),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  VakinhaButton(onPressed: (){}, label: 'ACESSAR', width: context.widthTransformer(reducedBy: 40))
+                ],
+              ),
+            )
+               ],
+             ),
+           ),
        );
   }
 }
